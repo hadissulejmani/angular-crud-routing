e358975a21a49673656f3209cafa43e33a155d11
Hadis Sulejmani <hadissulejmani@outlook.com>
2021-03-15 21:27:50 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './post/create/create.component';
+import { EditComponent } from './post/edit/edit.component';
+import { IndexComponent } from './post/index/index.component';
+import { ViewComponent } from './post/view/view.component';
 
 const routes: Routes = [];
 
